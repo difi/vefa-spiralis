4dd500e1628cffd9fd5706ac3181956c0b0668ba
Steinar Overbeck Cook <steinar.cook@gmail.com>
2017-03-08 09:50:45 +0100

    Fixed a failing test.
    Modified pom to prepare for a release

--- azureblob/src/test/java/no/balder/spiralis/payload/ContainerUtilTest.java
+++ azureblob/src/test/java/no/balder/spiralis/payload/ContainerUtilTest.java
@@ -30,13 +30,13 @@ public class ContainerUtilTest {
 
         final String containerNameForPayload = ContainerUtil.containerNameFor(Paths.get("/var/peppol/IN/9908_971589671/9908_848382922/2017-01-10/17524837-551a-4316-b3a3-feb9ebd84ac0.doc.xml"));
         assertNotNull(containerNameForPayload);
-        assertEquals(containerNameForPayload, "peppol-inbound");
+        assertEquals(containerNameForPayload, "peppol-ap");
 
         final String containerNameFor = ContainerUtil.containerNameFor(Paths.get("/var/peppol/IN/9908_971589671/9908_848382922/2017-01-10/17524837-551a-4316-b3a3-feb9ebd84ac0.receipt.dat"));
         assertEquals(containerNameForPayload, containerNameFor);
 
         final String s1 = ContainerUtil.containerNameFor(Paths.get("/Users/steinar/src/spiralis/azureblob/target/test-classes/sample-invoice.doc.xml"));
-        assertEquals(s1, "peppol-inbound");
+        assertEquals(s1, "peppol-ap");
 
     }
 
